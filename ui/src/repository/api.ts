eac571934c8e8bd2bc56b92180f13019a9c9ce0a
yoan <536464346@qq.com>
2024-08-23 13:22:44 +0800

    nixpacks

--- ui/src/repository/api.ts
+++ ui/src/repository/api.ts
@@ -1,7 +1,9 @@
 import PocketBase from "pocketbase";
+const apiDomain = import.meta.env.VITE_API_DOMAIN;
+console.log(apiDomain);
 let pb: PocketBase;
 export const getPb = () => {
   if (pb) return pb;
-  pb = new PocketBase("http://127.0.0.1:8090");
+  pb = new PocketBase(apiDomain ? apiDomain : "http://127.0.0.1:8090");
   return pb;
 };
