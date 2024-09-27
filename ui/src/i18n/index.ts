1e2d8fa0277fa1b9b129c9f2156389efe78c7c44
elvis liao <1219585136@qq.com>
2024-09-27 16:31:24 +0800

    chore: abstract resources configure

--- ui/src/i18n/index.ts
+++ ui/src/i18n/index.ts
@@ -2,24 +2,13 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import zh from './locales/zh.json'
-import en from './locales/en.json'
-
+import resources from './locales'
 
 i18n
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        resources: {
-            zh: {
-                name: '简体中文',
-                translation: zh
-            },
-            en: {
-                name: 'English',
-                translation: en
-            }
-        },
+        resources,
         fallbackLng: 'zh',
         debug: true,
         interpolation: {
---
+++ ui/src/i18n/locales/index.ts
@@ -0,0 +1,17 @@
+import { Resource } from 'i18next'
+
+import zh from './zh.json'
+import en from './en.json'
+
+const resources: Resource = {
+    zh: {
+        name: '简体中文',
+        translation: zh
+    },
+    en: {
+        name: 'English',
+        translation: en
+    }
+}
+
+export default resources;
