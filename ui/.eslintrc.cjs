b0923d54ee387a35391b1e2348fa2cda55ae53ce
Fu Diwei <fudiwei@sina.com>
2024-10-15 18:03:38 +0800

    chore: config vscode eslint

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -1,18 +1,23 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended", "plugin:react-hooks/recommended"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "react-refresh/only-export-components": [
+      "warn",
+      {
+        allowConstantExport: true,
+      },
     ],
   },
-}
+};
---
+++ ui/.prettierrc.cjs
@@ -0,0 +1,20 @@
+﻿/**
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  editorconfig: true,
+  htmlWhitespaceSensitivity: "ignore",
+  jsxSingleQuote: false,
+  endOfLine: "crlf",
+  printWidth: 160,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: false,
+  tabs: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+};
