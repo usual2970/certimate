bff18a7be7670b4a0ab2cc15472749d0a294dc14
yoan <536464346@qq.com>
2024-10-23 08:36:25 +0800

    update vite plugin to preserve special file

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,9 +1,38 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
+
+import fs from "fs-extra";
+
+const preserveFilesPlugin = (filesToPreserve: string[]): Plugin => {
+  return {
+    name: "preserve-files",
+    apply: "build",
+    buildStart() {
+      // 在构建开始时将要保留的文件或目录移动到临时位置
+      filesToPreserve.forEach((file) => {
+        const srcPath = path.resolve(__dirname, file);
+        const tempPath = path.resolve(__dirname, `temp_${file}`);
+        if (fs.existsSync(srcPath)) {
+          fs.moveSync(srcPath, tempPath, { overwrite: true });
+        }
+      });
+    },
+    closeBundle() {
+      // 在构建完成后将临时位置的文件或目录移回原来的位置
+      filesToPreserve.forEach((file) => {
+        const srcPath = path.resolve(__dirname, file);
+        const tempPath = path.resolve(__dirname, `temp_${file}`);
+        if (fs.existsSync(tempPath)) {
+          fs.moveSync(tempPath, srcPath, { overwrite: true });
+        }
+      });
+    },
+  };
+};
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), preserveFilesPlugin(["dist/.gitkeep"])],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
