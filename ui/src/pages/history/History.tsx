ea9e9165b636f853cefe237338b1019e93c2d83e
yoan <536464346@qq.com>
2024-10-24 21:03:57 +0800

    Fix the issue where  log information is not displayed.

--- ui/src/pages/history/History.tsx
+++ ui/src/pages/history/History.tsx
@@ -150,6 +150,10 @@ const History = () => {
                                   <div>[{item.time}]</div>
                                   <div className="ml-2">{item.message}</div>
                                 </div>
+                                {item.info &&
+                                  item.info.map((info: string) => {
+                                    return <div className="mt-1 text-green-600 break-words">{info}</div>;
+                                  })}
                                 {item.error && <div className="mt-1 text-red-600">{item.error}</div>}
                               </div>
                             );
