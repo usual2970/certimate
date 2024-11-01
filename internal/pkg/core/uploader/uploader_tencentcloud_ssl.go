1e3c4881d0ca39013acae8e42bcf8bb14f6e35f3
Fu Diwei <fudiwei@sina.com>
2024-11-01 15:33:02 +0800

    refactor: remove unused certificate name in TencentCloudSSLUploader

--- internal/pkg/core/uploader/uploader_tencentcloud_ssl.go
+++ internal/pkg/core/uploader/uploader_tencentcloud_ssl.go
@@ -2,8 +2,6 @@
 
 import (
 	"context"
-	"fmt"
-	"time"
 
 	xerrors "github.com/pkg/errors"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
@@ -37,14 +35,9 @@ func NewTencentCloudSSLUploader(config *TencentCloudSSLUploaderConfig) (Uploader
 }
 
 func (u *TencentCloudSSLUploader) Upload(ctx context.Context, certPem string, privkeyPem string) (res *UploadResult, err error) {
-	// 生成新证书名（需符合腾讯云命名规则）
-	var certId, certName string
-	certName = fmt.Sprintf("certimate-%d", time.Now().UnixMilli())
-
 	// 上传新证书
 	// REF: https://cloud.tencent.com/document/product/400/41665
 	uploadCertificateReq := tcSsl.NewUploadCertificateRequest()
-	uploadCertificateReq.Alias = common.StringPtr(certName)
 	uploadCertificateReq.CertificatePublicKey = common.StringPtr(certPem)
 	uploadCertificateReq.CertificatePrivateKey = common.StringPtr(privkeyPem)
 	uploadCertificateReq.Repeatable = common.BoolPtr(false)
@@ -53,22 +46,10 @@ func (u *TencentCloudSSLUploader) Upload(ctx context.Context, certPem string, pr
 		return nil, xerrors.Wrap(err, "failed to execute sdk request 'ssl.UploadCertificate'")
 	}
 
-	// 获取证书详情
-	// REF: https://cloud.tencent.com/document/api/400/41673
-	//
-	// P.S. 上传重复证书会返回上一次的证书 ID，这里需要重新获取一遍证书名（https://github.com/usual2970/certimate/pull/227）
-	describeCertificateDetailReq := tcSsl.NewDescribeCertificateDetailRequest()
-	describeCertificateDetailReq.CertificateId = uploadCertificateResp.Response.CertificateId
-	describeCertificateDetailResp, err := u.sdkClient.DescribeCertificateDetail(describeCertificateDetailReq)
-	if err != nil {
-		return nil, xerrors.Wrap(err, "failed to execute sdk request 'ssl.DescribeCertificateDetail'")
-	}
-
-	certId = *describeCertificateDetailResp.Response.CertificateId
-	certName = *describeCertificateDetailResp.Response.Alias
+	certId := *describeCertificateDetailResp.Response.CertificateId
 	return &UploadResult{
 		CertId:   certId,
-		CertName: certName,
+		CertName: "",
 	}, nil
 }
 
