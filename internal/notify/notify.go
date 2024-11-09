94579d65c4f0eb4deb6e2c365c64109dbb1b2aa3
Fu Diwei <fudiwei@sina.com>
2024-11-09 20:29:13 +0800

    refactor: clean code

--- internal/notify/notify.go
+++ internal/notify/notify.go
@@ -193,19 +193,3 @@ func getMailNotifier(conf map[string]any) (notifyPackage.Notifier, error) {
 
 	return rs, nil
 }
-
-func getString(conf map[string]any, key string) string {
-	if _, ok := conf[key]; !ok {
-		return ""
-	}
-
-	return conf[key].(string)
-}
-
-func getBool(conf map[string]any, key string) bool {
-	if _, ok := conf[key]; !ok {
-		return false
-	}
-
-	return conf[key].(bool)
-}
---
+++ internal/notify/utils.go
@@ -0,0 +1,17 @@
+﻿package notify
+
+func getString(conf map[string]any, key string) string {
+	if _, ok := conf[key]; !ok {
+		return ""
+	}
+
+	return conf[key].(string)
+}
+
+func getBool(conf map[string]any, key string) bool {
+	if _, ok := conf[key]; !ok {
+		return false
+	}
+
+	return conf[key].(bool)
+}
