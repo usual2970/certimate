668f6ee36fb25eb3e449cd6e37718dfaee22779f
Fu Diwei <fudiwei@sina.com>
2024-11-25 21:28:38 +0800

    feat(ui): use ant-design

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { ConfigProvider } from "antd";
+import AntdLocaleZhCN from "antd/locale/zh_CN";
+import "dayjs/locale/zh-cn";
 
 import { router } from "./router.tsx";
 import { ThemeProvider } from "./components/ThemeProvider.tsx";
 import "./i18n";
 import "./global.css";
 
+// TODO: antd i18n
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <ConfigProvider locale={AntdLocaleZhCN}>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </ConfigProvider>
   </React.StrictMode>
 );
