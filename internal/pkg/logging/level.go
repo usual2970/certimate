c13a7a7873f9b0f3ee903045ed8a1b2c5d6af0bc
Fu Diwei <fudiwei@sina.com>
2025-03-16 18:43:54 +0800

    feat: logging

---
+++ internal/pkg/logging/level.go
@@ -0,0 +1,31 @@
+﻿package logging
+
+import "log/slog"
+
+type Level string
+
+const (
+	LevelDebug Level = "DEBUG"
+	LevelInfo  Level = "INFO"
+	LevelWarn  Level = "WARN"
+	LevelError Level = "ERROR"
+)
+
+func (l Level) String() string {
+	return string(l)
+}
+
+func (l Level) Level() slog.Level {
+	switch l {
+	case LevelDebug:
+		return slog.LevelDebug
+	case LevelInfo:
+		return slog.LevelInfo
+	case LevelWarn:
+		return slog.LevelWarn
+	case LevelError:
+		return slog.LevelError
+	default:
+		return slog.Level(-1)
+	}
+}
---
+++ internal/pkg/logging/record.go
@@ -0,0 +1,14 @@
+﻿package logging
+
+import (
+	"time"
+
+	types "github.com/pocketbase/pocketbase/tools/types"
+)
+
+type Record struct {
+	Time    time.Time
+	Level   Level
+	Message string
+	Data    types.JSONMap[any]
+}
