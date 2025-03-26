4883b3bb882754aa9d24267bb7d2a32088f62145
Fu Diwei <fudiwei@sina.com>
2025-03-26 10:18:27 +0800

    feat(ui): make request error friendly

--- ui/src/utils/error.ts
+++ ui/src/utils/error.ts
@@ -2,7 +2,7 @@ import { ClientResponseError } from "pocketbase";
 
 export const getErrMsg = (error: unknown): string => {
   if (error instanceof ClientResponseError) {
-    return error.response != null ? getErrMsg(error.response) : error.message;
+    return Object.keys(error.response ?? {}).length ? getErrMsg(error.response) : error.message;
   } else if (error instanceof Error) {
     return error.message;
   } else if (typeof error === "object" && error != null) {
