eaf71e94dadb3cffeae0ee8dccb6730d695aa9b6
RHQYZ <fudiwei@sina.com>
2025-06-18 23:00:53 +0800

    build: set fallback app version to "v0.0.0-dev"

--- ui/src/domain/version.ts
+++ ui/src/domain/version.ts
@@ -1,3 +1,3 @@
 // .env > git tag > v0.0.0-beta
 // https://vite.dev/guide/env-and-mode.html#env-variables
-export const version = import.meta.env.VITE_APP_VERSION ?? __APP_VERSION__ ?? "v0.0.0-beta";
+export const version = import.meta.env.VITE_APP_VERSION ?? __APP_VERSION__ ?? "v0.0.0-dev";
